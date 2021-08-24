0195e7836273a66d398c7b6964556e60535f464a
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-08-24 13:43:00 +0200

    rest-client

--- public/js/newPlanet.js
+++ public/js/newPlanet.js
@@ -4,6 +4,9 @@ import makeRequest from './logic.js';
 
 let saveBtn = document.getElementById('saveBtn');
 saveBtn.addEventListener('click', async ()=> {
-    let response = await makeRequest('http://localhost:3000/savePlanets', 'POST', {name: "Mars", color: "red"});
+    let nameInput = document.getElementById('nameInput').value;
+    let colorInput = document.getElementById('colorInput').value;
+    console.log(`Name: ${nameInput} Color: ${colorInput}`)
+    let response = await makeRequest('http://localhost:3000/savePlanets', 'POST', {name: `${nameInput}`, color: `${colorInput}`});
     console.log(response)
 })
