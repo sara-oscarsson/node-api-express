8bc2719585412b09b58b851b6175d82f12faa943
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-08-24 11:01:33 +0200

    nasa-api

--- public/js/nasa.js
+++ public/js/nasa.js
@@ -1,7 +1,68 @@
+let sunBtn = document.getElementById('sun');
+let mercuryBtn = document.getElementById('mercury');
+let venusBtn = document.getElementById('venus');
+let earthBtn = document.getElementById('earth');
 let marsBtn = document.getElementById('mars');
-marsBtn.addEventListener('click', connectNasa('mars'));
+let jupiterBtn = document.getElementById('jupiter');
+let saturnBtn = document.getElementById('saturn');
+let uranusBtn = document.getElementById('uranus');
+let neptuneBtn = document.getElementById('neptune');
+let plutoBtn = document.getElementById('pluto');
+
+sunBtn.addEventListener('click', ()=> {
+    connectNasa('sun')
+});
+mercuryBtn.addEventListener('click', ()=> {
+    connectNasa('mercury')
+});
+venusBtn.addEventListener('click', ()=> {
+    connectNasa('venus')
+});
+earthBtn.addEventListener('click', ()=> {
+    connectNasa('earth')
+});
+marsBtn.addEventListener('click', ()=> {
+    connectNasa('planet mars')
+});
+jupiterBtn.addEventListener('click', ()=> {
+    connectNasa('jupiter')
+});
+saturnBtn.addEventListener('click', ()=> {
+    connectNasa('saturn')
+});
+uranusBtn.addEventListener('click', ()=> {
+    connectNasa('uranus')
+});
+plutoBtn.addEventListener('click', ()=> {
+    connectNasa('planet pluto')
+});
 
 
 async function connectNasa(planet) {
     console.log(`Hello from ${planet}`)
+    let mainDiv = document.getElementById('demo');
+    mainDiv.innerHTML = '';
+
+    let response = await fetch(`https://images-api.nasa.gov/search?q=${planet}&media_type=image`);
+    let result = await response.json();
+    
+    let listOfImages = result.collection.items
+    let shortListOfImages = listOfImages.slice(0, 8);
+
+    console.log(shortListOfImages)
+
+    shortListOfImages.map(i => {
+        console.log(i.links[0].href)
+        /* create img wrapper */
+        let imgWrapper = document.createElement('div');
+        imgWrapper.classList.add('imgWrapper');
+
+        /* create img */
+        let image = document.createElement('img');
+        image.src = i.links[0].href;
+
+        /* append */
+        imgWrapper.appendChild(image)
+        mainDiv.appendChild(imgWrapper)
+    })
 }
